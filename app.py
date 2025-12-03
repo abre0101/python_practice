ed8e58d4ea413560e7916c0bff49bd0b51ad40a9
Abraham Worku <Abrahamworku10a@gmail.com>
2025-12-03 11:40:14 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,86 @@
+# student_count = 1000
+# rating = 4.33
+# is_published = False
+# course_name = "Python Programming"
+# print("Total number of students:", student_count)
+
+# course = "Python Programming"
+# message = f"Course '{course}' has been published successfully!"
+# print(message)
+
+# course = "Python Programming"
+# print(len(course))
+# print(course[-1])
+# print(course[0:3])
+# print(course[0:])
+# print(course[:])
+# print(course[:3])
+
+# course = 'Python " Programming'
+# course = 'Python \" Programming'
+# course = 'Python \n Programming'
+# print(course)
+
+
+# first_name = "John"
+# last_name = "Doe"
+# full_name = first_name + " " + last_name
+# print(full_name)
+# full_name = F"{first_name} {last_name}"
+# print(full_name)
+# full_name = F"{len(first_name)} {last_name}"
+# print(full_name)
+
+# course = "     Python Programming"
+# print(course.upper())
+# print(course.lower())
+# print(course.title())
+# print(course.strip())
+# print(course.find("Pro"))
+# print(course.replace("P", "J"))
+# print("pro" in course)
+
+# x = 1
+# x = 1.1
+# x = 1+2j
+
+# print(10/3)
+# print(10//3)
+# print(10**3)
+
+# x = 10
+# x += 3
+# print(x)
+# import math
+# print(round(2.9))
+# print(abs(-2.9))
+# print(math.ceil(2.2))
+
+
+# x = input("Enter your name: ")
+
+# y = int(x)+1
+# print(y)
+# print(f"Hello {x}")
+# print("Hello" + x)
+
+# name = "abraham"
+# print(name[1:4])
+
+# temperature = 29
+# if temperature > 30:
+#     print("It's a hot day")
+
+# elif temperature < 30:
+#     print("It's a cold day")
+# else:
+#     print("It's a lovely day")
+# print("done")
+
+age = 20
+# if age >= 18:
+#     message = "You are eligible to vote"
+# else:
+#     message = "You are not eligible to vote"
+message = "You are eligible to vote" if age >= 18 else "You are not eligible to vote"
+print(message)
